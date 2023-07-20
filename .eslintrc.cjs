774bc9ecc5851631deda516582a34d75b85d3a68
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-20 10:34:26 +0300

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'eslint:all', 'plugin:react/all'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
