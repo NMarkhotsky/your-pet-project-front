b86f4dcd483440a92af8bc77fcb63d5129bc943f
Albina Anistratenko <albuska2013@gmail.com>
2023-07-20 21:26:09 +0300

    add header

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../Header/Header";
+
 function SharedLayout() {
-  return <h1>SharedLayout</h1>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default SharedLayout;
