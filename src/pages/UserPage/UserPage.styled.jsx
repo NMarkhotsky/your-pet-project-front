169a071297fee57a09aa1ef71022dbed8931c4d3
Albina Anistratenko <albuska2013@gmail.com>
2023-07-20 19:33:14 +0300

    add pets.json, start userForm

---
+++ src/pages/UserPage/UserPage.styled.jsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { Form, Field } from "formik";
+
+export const Container = styled.div`
+  background-color: #fef9f9;
+  padding: 100px;
+`;
+
+export const ImageBox = styled.div`
+  border-radius: 40px;
+  margin: 0 auto;
+  width: 182px;
+  height: 182px;
+  margin-bottom: 15px;
+  background-color: #fef9f9;
+`;
+
+export const ImageInputBox = styled.div`
+  text-align: center;
+  margin-bottom: 26px;
+`;
+
+export const FormBox = styled(Form)`
+  width: 395px;
+  padding: 20px 24px 16px;
+  background-color: #ffffff;
+  margin: 0 auto;
+  border-radius: 40px;
+`;
+
+export const InputBox = styled.div`
+  /* display: flex; */
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
+
+export const Label = styled.label`
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 0.72px;
+`;
+
+export const Input = styled(Field)`
+  /* float: right; */
+  outline: none;
+  border-radius: 20px;
+  border: 1px solid #54adff;
+  font-size: 16px;
+  letter-spacing: 0.64px;
+  padding: 4px 12px;
+`;
+
+export const ButtonForm = styled.button`
+  display: flex;
+  justify-content: center;
+  width: 255px;
+  border: none;
+  padding: 6px 0;
+  border-radius: 40px;
+  background: #54adff;
+  color: #fef9f9;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 0.64px;
+  margin-left: auto;
+`;
