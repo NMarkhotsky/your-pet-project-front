7c94b083de07f8f00e92f29ff96b4a53255ca5d7
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-20 17:24:27 +0300

    Added propTypes to component Icon.jsx

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,19 +1,28 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import icons from '../../assets/icons/sprite.svg';
 
-export const Icon = ({ iconName, width, stroke, fill }) => {
+export const Icon = ({ iconName, width, height, stroke, fill }) => {
   return (
-    <svg width={width || '24px'}>
+    <svg width={width || '24px'} height={height || '24px'}>
       <use href={`${icons}#${iconName}`} stroke={stroke} fill={fill} />
     </svg>
   );
 };
 
-/* 
+Icon.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  stroke: PropTypes.string,
+  fill: PropTypes.string,
+};
+
+/*
  Приклад використання компонента іконки
       <Icon
-        iconName={'icon-arrow-left'} назву іконки дивитись в src/assets/icons/sprite.svg і порівнювати з назвою на макеті
+        iconName={'icon-arrow-left'} // назву іконки дивитись в src/assets/icons/sprite.svg і порівнювати з назвою на макеті
         width={'24px'}   // якщо розміри 24px то не треба вказувати
+        height={'24px'} // якщо розміри 24px то не треба вказувати
         stroke={'#54ADFF'} // колір обводки
         fill={'#54ADFF'} // колір заливки
       />
