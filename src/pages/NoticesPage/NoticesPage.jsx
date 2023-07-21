7f623882a207059a46de88f084ff3ed4390a7336
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-21 12:25:46 +0300

    Update NoticesPage

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 function NoticesPage() {
-  return <h1>NoticesPage</h1>;
+  return (
+    <>
+      <h1>NoticesPage</h1>
+      <Outlet />
+    </>
+  );
 }
 
 export default NoticesPage;
