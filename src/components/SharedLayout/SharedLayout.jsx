b614749d154b53db0d34831b254595b8f8168439
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-21 12:23:55 +0300

    Added Container mob/tab/desc

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,16 @@
 // import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import AppRoutes from '../../routes/AppRoutes';
+import { Container } from './SharedLayout.styled';
 
 function SharedLayout() {
   return (
-    <>
+    <Container>
       <Header />
       <main>
         <AppRoutes />
       </main>
-    </>
+    </Container>
   );
 }
 
