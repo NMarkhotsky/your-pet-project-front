72113c93da252d01bc335ce8a06cbba97b791f49
Albina Anistratenko <albuska2013@gmail.com>
2023-07-22 15:39:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { GlobalStyle } from './components/GlobalStyle/GlobalStyle';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import UserPage from './pages/UserPage/UserPage';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <SharedLayout />
-      <UserPage/>
+ 
     </>
   );
 }
