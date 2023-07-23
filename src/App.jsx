b905e8a8e5b483e4986203117364d16595b7085e
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-23 15:56:10 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { GlobalStyle } from './components/GlobalStyle/GlobalStyle';
 import SharedLayout from './components/SharedLayout/SharedLayout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <SharedLayout />
       <GlobalStyle />
+      <ToastContainer autoClose={3000} />
     </>
   );
 }
