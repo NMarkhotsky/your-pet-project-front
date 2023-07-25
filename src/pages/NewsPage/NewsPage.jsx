1947ea467cbb43b2f92c97c1180980fb3115c2a1
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-25 19:25:17 +0300

    Update SearchInput

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import NewsList from './NewsList/NewsList';
 import allArticles from '../../temp/allArticles.json';
 import { Title } from './NewsPage.styled';
 import { SearchInput } from '../../shared/components/SearchInput/SearchInput';
 
 function NewsPage() {
+  const [searchValue, setSearchValue] = useState('');
 
-    return (
-      <>
-        <Title>News</Title>
-        <SearchInput />
-        <NewsList allArticles={allArticles.article} />
-      </>
-    )
+  const handleChange = value => {
+    setSearchValue(value);
+    console.log('Input Value:', value);
+  };
+
+  const handleSearch = () => {
+    console.log(searchValue);
+  };
+
+  const handleDelete = () => {
+    setSearchValue('');
+    console.log(searchValue);
+  };
+
+  return (
+    <>
+      <Title>News</Title>
+      <SearchInput
+        value={searchValue}
+        onChange={handleChange}
+        onSubmit={handleSearch}
+        onDelete={handleDelete}
+      />
+      <NewsList allArticles={allArticles.article} />
+    </>
+  );
 }
 
 export default NewsPage;
