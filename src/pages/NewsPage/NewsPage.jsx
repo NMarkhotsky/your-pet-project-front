c60a2dee1712800241e095bf9ce28cbc8c437416
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-07-25 18:40:05 +0300

    finish NewsPage

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,48 +1,17 @@
 import NewsList from './NewsList/NewsList';
 import allArticles from '../../temp/allArticles.json';
-import SearchForm from './SearchForm/SearchForm';
 import { Title } from './NewsPage.styled';
-import { Component } from 'react';
+import { SearchInput } from '../../shared/components/SearchInput/SearchInput';
 
-class NewsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowBtn: false,
-      inputValue: '',
-    }
-    this.showBtnClose = this.showBtnClose.bind(this);
-    this.clearInput = this.clearInput.bind(this);
-  }
+function NewsPage() {
 
-  clearInput(){
-    this.setState({isShowBtn: false, inputValue: ''})
-  }
-
-  showBtnClose(e) {
-    let inputText = e.currentTarget.value;
-
-    if (inputText) {
-      this.setState({isShowBtn: true, inputValue: inputText})
-    } else {
-      this.setState({isShowBtn: false, inputValue: ''})
-    }
-  }
-
-  render() {
     return (
       <>
         <Title>News</Title>
-        <SearchForm 
-          isShowBtn={this.state.isShowBtn}
-          showBtnClose={this.showBtnClose}
-          clearInput={this.clearInput}
-          inputValue={this.state.inputValue}
-        />
+        <SearchInput />
         <NewsList allArticles={allArticles.article} />
       </>
     )
-  }
-  }
+}
 
 export default NewsPage;
