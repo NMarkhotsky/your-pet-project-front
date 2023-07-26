a36aa951b189af71fef2c08f6953e346cbd1738f
Albina Anistratenko <albuska2013@gmail.com>
2023-07-26 09:05:21 +0300

    Update PetsList.jsx

--- src/components/PetsList/PetsList.jsx
+++ src/components/PetsList/PetsList.jsx
@@ -10,9 +10,21 @@ import { PetsItem } from '../PetsItem/PetsItem';
 import { Icon } from '../Icon/Icon';
 import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react';
+import {
+  Title,
+  TitleBr,
+  CatImg,
+  Img,
+} from '../../pages/NotFoundPage/NotFoundPage.styled';
+
+import catdesktop1x from '../../assets/images/NotFoundImages/catdesktop.png';
+import catdesktop2x from '../../assets/images/NotFoundImages/catdesktop@2x.png';
+import cattablet1x from '../../assets/images/NotFoundImages/cattablet.png';
+import cattablet2x from '../../assets/images/NotFoundImages/cattablet@2x.png';
+import catmobile1x from '../../assets/images/NotFoundImages/catmobile.png';
+import catmobile2x from '../../assets/images/NotFoundImages/catmobile@2x.png';
 
 export const PetsList = ({ pets, deletePet }) => {
-
   useEffect(() => {
     if (pets === [] && !pets) {
       return;
@@ -42,6 +54,30 @@ export const PetsList = ({ pets, deletePet }) => {
           <PetsItem key={card._id} item={card} deletePet={deletePet} />
         ))}
       </PetsCardList>
+      {pets.length === 0 && (
+        <div>
+          <Title>
+            <TitleBr>Sorry!</TitleBr> You don't have any pets &#58;&#40;
+          </Title>
+          <CatImg>
+            <picture>
+              <source
+                srcSet={(catdesktop1x, catdesktop2x)}
+                media="(min-width: 1280px)"
+              />
+              <source
+                srcSet={(cattablet1x, cattablet2x)}
+                media="(min-width: 768px) and (max-width: 1279px)"
+              />
+              <source
+                srcSet={(catmobile1x, catmobile2x)}
+                media="(max-width: 767px)"
+              />
+              <Img src={catdesktop1x} alt="Cat with tongue" />
+            </picture>
+          </CatImg>
+        </div>
+      )}
     </ContainerList>
   );
 };
