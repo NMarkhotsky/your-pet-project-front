9f9847764415f3f547bd4f326ac178bf288b2e70
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-26 17:09:09 +0300

    Optimization images, added background images to pages

--- src/shared/components/BackgroundWrapper/BackgroundWrapper.styled.jsx
+++ src/shared/components/BackgroundWrapper/BackgroundWrapper.styled.jsx
@@ -4,39 +4,45 @@ import {
   selectTablet,
   selectDesktop,
 } from '../../../utils/selectMediaRequests';
-import bgdesktop from '../../../assets/images/background/desktop/bg_desktop.png';
-import bgdesktop2x from '../../../assets/images/background/desktop/bg_desktop@2x.png';
-import bgtablet from '../../../assets/images/background/tablet/bg_tablet.png';
-import bgtablet2x from '../../../assets/images/background/tablet/bg_tablet@2x.png';
-import bgmobile from '../../../assets/images/background/mobile/bg_mobile.png';
-import bgmobile2x from '../../../assets/images/background/mobile/bg_mobile@2x.png';
+import bgdesktop from '../../../assets/images/background/desktop/bg_desktop.webp';
+import bgdesktop2x from '../../../assets/images/background/desktop/bg_desktop@2x.webp';
+import bgtablet from '../../../assets/images/background/tablet/bg_tablet.webp';
+import bgtablet2x from '../../../assets/images/background/tablet/bg_tablet@2x.webp';
+import bgmobile from '../../../assets/images/background/mobile/bg_mobile.webp';
+import bgmobile2x from '../../../assets/images/background/mobile/bg_mobile@2x.webp';
 
 export const BackgrdImageContainer = styled.div`
   background-image: url(${bgmobile});
   background-repeat: no-repeat;
   background-size: cover;
-  background-position-y: -68px;
 
-  height: 100%;
+  height: calc(100vh - 50px);
   @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
     background-image: url(${bgmobile2x});
   }
 
   @media ${selectTablet} {
     width: 720px;
-    height: 1125px;
+    height: calc(100vh - 72px);
     background-image: url(${bgtablet});
+
+    background-position-y: -68px;
+
     @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
       background-image: url(${bgtablet2x});
+      background-position-y: -68px;
     }
   }
 
   @media ${selectDesktop} {
     background-image: url(${bgdesktop});
+    background-position-y: -68px;
+
     width: 1280px;
-    height: 702px;
+    height: calc(100vh - 70px);
     @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
       background-image: url(${bgdesktop2x});
+      background-position-y: -68px;
     }
   }
 `;
