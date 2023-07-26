00722daf7f5b3ff991963934a3218b6b1ff831ec
Maria Popova <0979867251m@gmail.com>
2023-07-26 04:14:56 +0300

    backgroundImg

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { Btn } from '../../shared/components/Button/Btn';
 import { Icon } from '../../components/Icon/Icon';
 import { Title, TitleBr, CatImg, Img } from './NotFoundPage.styled';
+import { BackgrdImg } from '../../shared/components/BackgroundImage/BackgroundImage.styled';
 
 import catdesktop1x from '../../assets/images/NotFoundImages/catdesktop.png';
 import catdesktop2x from '../../assets/images/NotFoundImages/catdesktop@2x.png';
@@ -12,39 +13,41 @@ import catmobile2x from '../../assets/images/NotFoundImages/catmobile@2x.png';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <Title>
-        <TitleBr>Ooops!</TitleBr> This page not found &#58;&#40;
-      </Title>
-      <CatImg>
-        <picture>
-          <source
-            srcSet={(catdesktop1x, catdesktop2x)}
-            media="(min-width: 1280px)"
-          />
-          <source
-            srcSet={(cattablet1x, cattablet2x)}
-            media="(min-width: 768px) and (max-width: 1279px)"
-          />
-          <source
-            srcSet={(catmobile1x, catmobile2x)}
-            media="(max-width: 767px)"
-          />
-          <Img src={catdesktop1x} alt="Cat with tongue" />
-        </picture>
-      </CatImg>
-      <NavLink to="/">
-        <Btn>
-          To main page
-          <Icon
-            iconName={'icon-pawprint'}
-            width={'24px'}
-            height={'24px'}
-            fill={'#54ADFF'}
-          />
-        </Btn>
-      </NavLink>
-    </div>
+    <BackgrdImg>
+      <div>
+        <Title>
+          <TitleBr>Ooops!</TitleBr> This page not found &#58;&#40;
+        </Title>
+        <CatImg>
+          <picture>
+            <source
+              srcSet={(catdesktop1x, catdesktop2x)}
+              media="(min-width: 1280px)"
+            />
+            <source
+              srcSet={(cattablet1x, cattablet2x)}
+              media="(min-width: 768px) and (max-width: 1279px)"
+            />
+            <source
+              srcSet={(catmobile1x, catmobile2x)}
+              media="(max-width: 767px)"
+            />
+            <Img src={catdesktop1x} alt="Cat with tongue" />
+          </picture>
+        </CatImg>
+        <NavLink to="/">
+          <Btn>
+            To main page
+            <Icon
+              iconName={'icon-pawprint'}
+              width={'24px'}
+              height={'24px'}
+              fill={'#54ADFF'}
+            />
+          </Btn>
+        </NavLink>
+      </div>
+    </BackgrdImg>
   );
 };
 export default NotFoundPage;
---
+++ src/shared/components/BackgroundImage/BackgroundImage.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import {
+  selectTablet,
+  selectDesktop,
+} from '../../../utils/selectMediaRequests';
+import bgdesktop from '../../../assets/images/background/desktop/bg_desktop.png';
+import bgdesktop2x from '../../../assets/images/background/desktop/bg_desktop@2x.png';
+import bgtablet from '../../../assets/images/background/tablet/bg_tablet.png';
+import bgtablet2x from '../../../assets/images/background/tablet/bg_tablet@2x.png';
+import bgmobile from '../../../assets/images/background/mobile/bg_mobile.png';
+import bgmobile2x from '../../../assets/images/background/mobile/bg_mobile@2x.png';
+export const BackgrdImg = styled.div`
+  background-image: url(${bgmobile});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: -68px;
+  width: 320px;
+  height: 533px;
+  @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+    background-image: url(${bgmobile2x});
+  }
+
+  @media ${selectTablet} {
+    width: 768px;
+    height: 1125px;
+    background-image: url(${bgtablet});
+    @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url(${bgtablet2x});
+    }
+  }
+
+  @media ${selectDesktop} {
+    background-image: url(${bgdesktop});
+    width: 1280px;
+    height: 702px;
+    @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url(${bgdesktop2x});
+    }
+  }
+`;
