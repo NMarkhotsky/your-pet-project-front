0e5301ac3cdd74078074ebc4386e22968daa19db
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-07-28 03:51:55 +0200

    Add loader styles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,14 @@
 import { Header } from '../Header/Header';
 import AppRoutes from '../../routes/AppRoutes';
 import { Main } from './SharedLayout.styled';
+import { useAuth } from '../../hooks/useAuth/useAuth';
+import { Loader } from '../../shared/components/Loader/Loader';
 
 function SharedLayout() {
+  const { isRefreshing } = useAuth();
   return (
     <>
+      {isRefreshing ? <Loader /> : null}
       <Header />
       <Main>
         <AppRoutes />
--- src/shared/components/Loader/Loader.styled.jsx
+++ src/shared/components/Loader/Loader.styled.jsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
+
 export const ImgLoader = styled.img`
-  margin: auto;
+  position: absolute;
+  top: calc(50% - 100px);
+  left: calc(50% - 100px);
 `;
 
-export const LoaderStyled = styled.div`
-  height: 300px;
-  width: 300px;
-  margin: auto;
-  align-items: center;
+export const LoaderOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  background-color: rgba(0, 0, 0, 0.2);
+  transition: opacity 500ms linear, visibility 500ms linear;
+  z-index: 999;
 `;
