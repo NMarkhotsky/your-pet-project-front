ea4e07d32d2cbb80e5c1efe8eaf8f8c851ed6473
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-07-30 21:20:01 +0300

    UPDATED: App.jsx (useEffect + useSearchParams) + authSlice.js(Added googleAuth reducer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,15 +12,24 @@ import { darkTheme, lightTheme } from './theme/theme';
 import { useFont } from './hooks/useFonts';
 import { fonts } from './constants/fonts';
 import { Loader } from './shared/components/Loader/Loader';
+import { googleAuth } from './redux/auth/authSlice';
 
 function App() {
   const fontsLoaded = useFont(fonts);
   const currentTheme = useTheme();
   const dispatch = useDispatch();
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const token = searchParams.get('token');
+
   useEffect(() => {
+    if (token) {
+      dispatch(googleAuth(token));
+      setSearchParams('');
+    }
+
     dispatch(fetchCurrentUser());
-  }, [dispatch]);
+  }, [dispatch, setSearchParams, token]);
 
   return fontsLoaded ? (
     <ThemeProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
