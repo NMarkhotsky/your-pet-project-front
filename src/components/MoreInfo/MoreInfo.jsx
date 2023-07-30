6b0f09d18b3a4f9fb1e777fa670c2372784f0c4b
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-07-30 21:42:38 +0300

    Add PetPage Navigation

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -1,22 +1,27 @@
 /* eslint-disable react/prop-types */
-import { useFormik } from "formik";
-import { petValues } from "../../constants";
+import { useFormik } from 'formik';
+import { petValues } from '../../constants';
 
-import DefaultMoreInfo from "../DefaultMoreInfo/DefaultMoreInfo";
-import SellMoreInfo from "../SellMoreInfo/SellMoreInfo";
-import YourPetMoreInfo from "../YourPetMoreInfo/YourPetMoreInfo";
-import { validationYourPet } from "../YourPetMoreInfo/validationYourPet";
-import { useAddPet } from "../../hooks";
-import { validationDefaultInfo } from "../DefaultMoreInfo/validationDefaultInfo";
-import { validationSellInfo } from "../SellMoreInfo/validationSellInfo";
-import { useDispatch } from "react-redux";
-import { addNotice, addPet } from "../../redux/add-pet/operations";
-import { clearMoreInfo } from "../../redux/add-pet/moreInfoSlice";
-import { clearOption } from "../../redux/add-pet/optionSlice";
-import { clearPersonalDetails } from "../../redux/add-pet/personalDetailsSlice";
+import DefaultMoreInfo from '../DefaultMoreInfo/DefaultMoreInfo';
+import SellMoreInfo from '../SellMoreInfo/SellMoreInfo';
+import YourPetMoreInfo from '../YourPetMoreInfo/YourPetMoreInfo';
+import { validationYourPet } from '../YourPetMoreInfo/validationYourPet';
+import { useAddPet } from '../../hooks';
+import { validationDefaultInfo } from '../DefaultMoreInfo/validationDefaultInfo';
+import { validationSellInfo } from '../SellMoreInfo/validationSellInfo';
+import { useDispatch } from 'react-redux';
+import { addNotice, addPet } from '../../redux/add-pet/operations';
+import { clearMoreInfo } from '../../redux/add-pet/moreInfoSlice';
+import { clearOption } from '../../redux/add-pet/optionSlice';
+import { clearPersonalDetails } from '../../redux/add-pet/personalDetailsSlice';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function MoreInfo({ option, handleDefinePage, setFile, file }) {
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
   const { personalDetails } = useAddPet();
+  const location = useLocation();
 
   const dispatch = useDispatch();
 
@@ -34,22 +39,26 @@ function MoreInfo({ option, handleDefinePage, setFile, file }) {
       dispatch(clearPersonalDetails());
       dispatch(clearMoreInfo());
       dispatch(clearOption());
+      setIsFormSubmitted(true);
       return;
     }
 
     const { type, gender, ...noticeObj } = data;
 
     if (option === petValues.SELL) {
-      dispatch(addNotice({
-        noticeType: option,
-        sex: data.gender,
-        petType: data.type,
-        ...noticeObj
-      }))
+      dispatch(
+        addNotice({
+          noticeType: option,
+          sex: data.gender,
+          petType: data.type,
+          ...noticeObj,
+        })
+      );
       resetForm();
       dispatch(clearPersonalDetails());
       dispatch(clearMoreInfo());
       dispatch(clearOption());
+      setIsFormSubmitted(true);
       return;
     }
 
@@ -60,22 +69,25 @@ function MoreInfo({ option, handleDefinePage, setFile, file }) {
     console.log(gender);
     console.log(price);
 
-    dispatch(addNotice({
-      noticeType: option,
-      sex: data.gender,
-      petType: data.type,
-      ...noticeObjWithoutPrice
-    }))
+    dispatch(
+      addNotice({
+        noticeType: option,
+        sex: data.gender,
+        petType: data.type,
+        ...noticeObjWithoutPrice,
+      })
+    );
     dispatch(clearPersonalDetails());
     dispatch(clearMoreInfo());
     dispatch(clearOption());
+    setIsFormSubmitted(true);
     resetForm();
-  }
+  };
 
   const formikYourPet = useFormik({
     initialValues: {
       photo: null,
-      comments: "",
+      comments: '',
     },
     onSubmit,
     validationSchema: validationYourPet,
@@ -83,11 +95,11 @@ function MoreInfo({ option, handleDefinePage, setFile, file }) {
 
   const formikSellPet = useFormik({
     initialValues: {
-      gender: "",
+      gender: '',
       photo: null,
-      location: "",
-      price: "",
-      comments: "",
+      location: '',
+      price: '',
+      comments: '',
     },
     onSubmit,
     validationSchema: validationSellInfo,
@@ -95,24 +107,48 @@ function MoreInfo({ option, handleDefinePage, setFile, file }) {
 
   const formikDefault = useFormik({
     initialValues: {
-      gender: "",
+      gender: '',
       photo: null,
-      location: "",
-      comments: "",
+      location: '',
+      comments: '',
     },
     onSubmit,
     validationSchema: validationDefaultInfo,
   });
 
-  switch (option) {
-    case petValues.yourPet:
-      return <YourPetMoreInfo setFile={setFile} file={file} formik={formikYourPet} handleDefinePage={handleDefinePage} />
-    case petValues.SELL:
-      return <SellMoreInfo setFile={setFile} file={file} formik={formikSellPet} handleDefinePage={handleDefinePage} />
-    default:
-      return <DefaultMoreInfo setFile={setFile} file={file} formik={formikDefault} handleDefinePage={handleDefinePage} />
+  if (isFormSubmitted) {
+    return <Navigate to={location.state.from.pathname} />;
   }
 
+  switch (option) {
+    case petValues.yourPet:
+      return (
+        <YourPetMoreInfo
+          setFile={setFile}
+          file={file}
+          formik={formikYourPet}
+          handleDefinePage={handleDefinePage}
+        />
+      );
+    case petValues.SELL:
+      return (
+        <SellMoreInfo
+          setFile={setFile}
+          file={file}
+          formik={formikSellPet}
+          handleDefinePage={handleDefinePage}
+        />
+      );
+    default:
+      return (
+        <DefaultMoreInfo
+          setFile={setFile}
+          file={file}
+          formik={formikDefault}
+          handleDefinePage={handleDefinePage}
+        />
+      );
+  }
 }
 
 export default MoreInfo;
