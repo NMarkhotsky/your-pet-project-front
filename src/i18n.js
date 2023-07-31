27e7e5ae89a5c6f1bd752c8161a9853e466e0368
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-08-01 00:39:45 +0300

    Fixed: url for i18nhttp

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,14 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-i18next.use(initReactI18next).use(LanguageDetector).use(Backend).init({
-  debug: true,
-  fallbackLng: 'en',
-});
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(Backend)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/your-pet-project-front/locales/{{lng}}/{{ns}}.json',
+    },
+  });
