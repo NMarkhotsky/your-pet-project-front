45991b89c9426c08798449214a0712362ac02cf0
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-07-31 23:13:50 +0300

    del: comments

--- src/components/NoticeCard/NoticeCard.jsx
+++ src/components/NoticeCard/NoticeCard.jsx
@@ -20,53 +20,6 @@ import { Btn } from '../../shared/components/Button/Btn';
 import { NoticeCardDetail } from '../NoticeCardDetail/NoticeCardDetail';
 import { useState } from 'react';
 
-// export const NoticeCard = ({ items }) => {
-//   return (
-//     <List>
-//       {items.pets.map(pet => (
-//         <Item key={pet.name}>
-//           <ContainerPetInfo>
-//             <Image src={pet.url} alt="pet" loading="lazy"></Image>
-//             <ContainerPetStatus>
-//               <TextStatus>{pet.status}</TextStatus>
-//               <ContainerButton>
-//                 <Button aria-label="add to favorites">
-//                   <Icon
-//                     iconName={'icon-heart'}
-//                     width={'24px'}
-//                     height={'24px'}
-//                     stroke={'#54ADFF'}
-//                     fill={'#54ADFF'}
-//                   />
-//                 </Button>
-//               </ContainerButton>
-//             </ContainerPetStatus>
-//             <ListPetInfo>
-//               <ItemPetInfo>
-//                 <Icon iconName={'icon-location'} />
-//                 <SpanPetText>{truncateText(pet.place)}</SpanPetText>
-//               </ItemPetInfo>
-//               <ItemPetInfo>
-//                 <Icon iconName={'icon-clock'} />
-//                 <SpanPetText>{convertToYears(pet.birthday)}</SpanPetText>
-//               </ItemPetInfo>
-//               <ItemPetInfo>
-//                 <Icon
-//                   iconName={pet.sex === 'Female' ? 'icon-female' : 'icon-male'}
-//                 />
-//                 <SpanPetText>{pet.sex}</SpanPetText>
-//               </ItemPetInfo>
-//             </ListPetInfo>
-//           </ContainerPetInfo>
-
-//           <TextPetName>{pet.name}</TextPetName>
-
-//           <Btn>Learn more</Btn>
-//         </Item>
-//       ))}
-//     </List>
-//   );
-// };
 export const NoticeCard = ({ item }) => {
   const [isLearnMore, setIsLearnMore] = useState(false)
 
