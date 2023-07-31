9dc5f31afe52b017061e609dfbb38e62395a68d1
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-07-31 13:57:54 +0300

    add default value today

--- src/constants/initialStatePersonalDetails.js
+++ src/constants/initialStatePersonalDetails.js
@@ -1,6 +1,8 @@
+import { format } from 'date-fns';
+
 export const initialStatePersonalDetails = {
   name: "",
-  birthday: "2020-07-31",
+  birthday: format(new Date(), 'yyyy-MM-dd'),
   type: "",
 }
 
