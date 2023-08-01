1988aa669cbffd6f7b5250ec847527079035f71a
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-08-01 16:00:11 +0300

    page notices styles

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,13 +1,11 @@
 import PropTypes, { shape } from 'prop-types';
 import { NoticeCard } from '../NoticeCard/NoticeCard';
-import { Pagination } from '../Pagination/Pagination';
 import { List } from '../NoticeCard/NoticeCard.styled';
 
-const NoticesCategoriesList = ({ notices, pageCount, handlePageChange }) => {
+const NoticesCategoriesList = ({ notices }) => {
 
   return <List>
-    {notices && notices.map(item => <NoticeCard key={item.id} item={item} />)}
-    <Pagination pageCount={pageCount} handlePageChange={handlePageChange}/>
+      {notices && notices.map(item => <NoticeCard key={item.id} item={item} />)}
   </List>;
 };
 
@@ -17,6 +15,4 @@ NoticesCategoriesList.propTypes = {
   notices: PropTypes.arrayOf(shape({
     id: PropTypes.string.isRequired,
   })).isRequired,
-  pageCount: PropTypes.number.isRequired,
-  handlePageChange: PropTypes.func.isRequired,
 };
