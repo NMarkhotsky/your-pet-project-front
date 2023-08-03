e8d6d1d943aadf6b1a275aff45df2c0701a8962e
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-08-03 14:43:50 +0300

    Fix validation files

--- src/constants/basicValidationMoreInfo.js
+++ src/constants/basicValidationMoreInfo.js
@@ -1,22 +1,28 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 const MAX_SIZE_FILE = 3;
 const MAX_LENGTH_MESSAGE = 70;
 
 export const basicValidationMoreInfo = {
-  photo: Yup
-    .mixed()
-    .required("Pet photo is required")
-    .test("fileFormat", "Only JPG files are allowed", (value) => {
+  photo: Yup.mixed()
+    .required('Pet photo is required')
+    .test('fileFormat', 'Only JPG files are allowed', value => {
       if (!value) return false;
-      return value.type === "image/jpeg";
+      return value.type;
+      // return value.type === 'image/jpeg';
     })
-    .test("fileSize", `File size should not exceed ${MAX_SIZE_FILE} MB`, (value) => {
-      if (!value) return false;
-      return value.size <= MAX_SIZE_FILE * 1024 * 1024;
-    }),
-  comments: Yup
-    .string()
-    .max(MAX_LENGTH_MESSAGE, `Maximum message length - ${MAX_LENGTH_MESSAGE} characters`)
-    .required("Comments are required"),
-}
+    .test(
+      'fileSize',
+      `File size should not exceed ${MAX_SIZE_FILE} MB`,
+      value => {
+        if (!value) return false;
+        return value.size <= MAX_SIZE_FILE * 1024 * 1024;
+      }
+    ),
+  comments: Yup.string()
+    .max(
+      MAX_LENGTH_MESSAGE,
+      `Maximum message length - ${MAX_LENGTH_MESSAGE} characters`
+    )
+    .required('Comments are required'),
+};
